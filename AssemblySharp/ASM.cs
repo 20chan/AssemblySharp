c1f4cfd3fa3c0f19aa8cf1a457aa67e716ae277b
phillyai <sonagi.philly@gmail.com>
2017-09-28 18:31:27 +0900

    레지스터, 어셈블리 열거형 추가

---
+++ AssemblySharp/ASM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblySharp
+{
+    public enum ASM
+    {
+        #region Data Movement
+        mov,
+        push,
+        pop,
+        lea,
+        #endregion
+        #region Arithmetic and Logic
+        add,
+        sub,
+        inc,
+        dec,
+        imul,
+        idiv,
+        and,
+        or,
+        xor,
+        not,
+        neg,
+        shl,
+        shr,
+        #endregion
+        #region Control Flow
+        jmp,
+        je, jne, jz, jg, jge, jl, jle,
+        cmp,
+        call,
+        ret,
+        #endregion
+    }
+}
---
+++ AssemblySharp/REG.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblySharp
+{
+    /// <summary>
+    /// Registers
+    /// </summary>
+    public enum REG
+    {
+        // reg8
+        AH,
+        BH,
+        CH,
+        DH,
+        AL,
+        BL,
+        CL,
+        DL,
+        // reg16
+        AX,
+        BX,
+        CX,
+        DX,
+        // reg32
+        EAX,
+        EBX,
+        ECX,
+        EDX,
+        ESI,
+        EDI,
+        ESP,
+        EBP,
+        // reg64
+        RAX,
+        RBX,
+        RCX,
+        RDX,
+        RBP,
+        RSI,
+        RDI,
+        RSP,
+    }
+}
