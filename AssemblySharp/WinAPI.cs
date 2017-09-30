20febad660745a1a5e4f8266d707a2ab9a572973
phillyai <sonagi.philly@gmail.com>
2017-09-30 20:40:04 +0900

    RunMachineCode 구현

---
+++ AssemblySharp/WinAPI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AssemblySharp
+{
+    internal static class WinAPI
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
+
+        [Flags]
+        public enum AllocationType
+        {
+            Commit = 0x1000,
+            Reserve = 0x2000,
+            Decommit = 0x4000,
+            Release = 0x8000,
+            Reset = 0x80000,
+            Physical = 0x400000,
+            TopDown = 0x100000,
+            WriteWatch = 0x200000,
+            LargePages = 0x20000000
+        }
+
+        [Flags]
+        public enum MemoryProtection
+        {
+            Execute = 0x10,
+            ExecuteRead = 0x20,
+            ExecuteReadWrite = 0x40,
+            ExecuteWriteCopy = 0x80,
+            NoAccess = 0x01,
+            ReadOnly = 0x02,
+            ReadWrite = 0x04,
+            WriteCopy = 0x08,
+            GuardModifierflag = 0x100,
+            NoCacheModifierflag = 0x200,
+            WriteCombineModifierflag = 0x400
+        }
+        const int
+            PAGE_READWRITE = 0x40,
+            PROCESS_VM_OPERATION = 0x0008,
+            PROCESS_VM_READ = 0x0010,
+            PROCESS_VM_WRITE = 0x0020;
+
+        const uint PAGE_EXECUTE_READWRITE = 0x40;
+    }
+}
--- AssemblySharp/X86Assembly.cs
+++ AssemblySharp/X86Assembly.cs
@@ -3,26 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace AssemblySharp
 {
+    public delegate object ObjectDelegate();
     public static class X86Assembly
     {
         public static object ExecuteScript(params object[] code)
         {
             // 델리게이트를 리턴해야 할텐데 그 리턴 타입을 지정해서 줘야 겠지
             // 아마 제너릭을 사용해서 어떻게든 잘 해봐야 하지 않을까
+            // 일단은 ObjectDelegate로 object로 모든걸 하는 걸루
             throw new NotImplementedException();
         }
 
         public static byte[] CompileToMachineCode(string asmcode)
         {
+            // objdump를 사용하는 게 괜찮을 거 같은데...
             throw new NotImplementedException();
         }
 
-        public static void RunMachineCode(byte[] bytecode)
+        public static object RunMachineCode(byte[] bytecode)
         {
-            throw new NotImplementedException();
+            return CompileMachineCode(bytecode)();
+        }
+
+        public static ObjectDelegate CompileMachineCode(byte[] bytecode)
+        {
+            var buffer = WinAPI.VirtualAlloc(IntPtr.Zero, (uint)bytecode.Length, WinAPI.AllocationType.Commit, WinAPI.MemoryProtection.ExecuteReadWrite);
+            Marshal.Copy(bytecode, 0, buffer, bytecode.Length);
+            return Marshal.GetDelegateForFunctionPointer<ObjectDelegate>(buffer);
         }
     }
 }
