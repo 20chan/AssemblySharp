41ad5ff4b03fbd4f564a9a1300147d5ca5a275ee
phillyai <sonagi.philly@gmail.com>
2017-12-12 19:43:11 +0900

    Emulator 틀 추가

---
+++ AssemblySharp/Machine/DynamicInvoke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblySharp.Machine
+{
+    /// <summary>
+    /// Execute machine code by allocating memory.
+    /// </summary>
+    public class DynamicInvoke : Emulator
+    {
+    }
+}
---
+++ AssemblySharp/Machine/VM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblySharp.Machine
+{
+    /// <summary>
+    /// X86 Virtual Machine
+    /// </summary>
+    public class VM
+    {
+    }
+}
