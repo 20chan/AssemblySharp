f45a005c625353cbb62b7b02550764146018f018
phillyai <sonagi.philly@gmail.com>
2017-12-14 04:29:12 +0900

    VM 틀 잡음
    Execute와 같은 메서드 인터페이스 추가, 레지스터 추가

--- AssemblySharp/Machine/VM.cs
+++ AssemblySharp/Machine/VM.cs
@@ -11,5 +11,34 @@ namespace AssemblySharp.Machine
     /// </summary>
     public class VM
     {
+        #region Registers
+        public class RegisterManager
+        {
+            private VM vm;
+            public int EAX => vm.EAX;
+
+            public RegisterManager(VM vm)
+            {
+                this.vm = vm;
+            }
+        }
+        public RegisterManager Registers { get; private set; }
+        private int EAX;
+        #endregion
+        public VM()
+        {
+            Registers = new RegisterManager(this);
+            
+        }
+
+        public object ExecuteFunction(byte[] codes, Type funcType, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteCode(params byte[] codes)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ AssemblySharpTests/Machine/VMTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblySharp.Machine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblySharp.Machine.Tests
+{
+    [TestClass()]
+    public class VMTests
+    {
+        [TestMethod()]
+        public unsafe void VMTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ExecuteFunctionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExecuteCodeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
